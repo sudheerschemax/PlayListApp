d1e1ce69e6dc377aa3606f16e320c0bbdfac8f43
sudheer <sudheer@schemaxtech.com>
2020-05-08 23:15:14 +0530

    #code Indentation

--- frontend/src/components/servicecomponents/CreatePlayList/CreatePlayList.tsx
+++ frontend/src/components/servicecomponents/CreatePlayList/CreatePlayList.tsx
@@ -1,146 +1,167 @@
-import React, { useEffect, useState } from 'react';
-import { PlayListService } from '../../../services/PlayListService';
-import { Card, Avatar, Pagination, Row, Col, Button, Tag, Form, Input, Table, Alert, message } from 'antd';
-import Meta from 'antd/lib/card/Meta';
-import { FilterUtil } from '../../../utils/filter-util';
+import React, { useEffect, useState } from "react";
+import { PlayListService } from "../../../services/PlayListService";
+import {
+  Card,
+  Avatar,
+  Pagination,
+  Row,
+  Col,
+  Button,
+  Tag,
+  Form,
+  Input,
+  Table,
+  Alert,
+  message
+} from "antd";
+import Meta from "antd/lib/card/Meta";
+import { FilterUtil } from "../../../utils/filter-util";
 export interface CreatePlaylistProps {}
-const CreatePlaylist = (props :CreatePlaylistProps) => {
-    const libraryServive = new PlayListService();
-    const filterUtil = new FilterUtil();
+const CreatePlaylist = (props: CreatePlaylistProps) => {
+  const libraryServive = new PlayListService();
+  const filterUtil = new FilterUtil();
 
-    const [songsLibrary, setsongsLibrary] = useState([]);
-    const [selectedsongsLibrary, setselectedsongsLibrary] = useState([]);
-    const [album, setalbum] = useState<string>('');
-    const [filtered, setfiltered] = useState([]);
-    const [status, setstatus] = useState<boolean>();
+  const [songsLibrary, setsongsLibrary] = useState([]);
+  const [selectedsongsLibrary, setselectedsongsLibrary] = useState([]);
+  const [album, setalbum] = useState<string>("");
+  const [filtered, setfiltered] = useState([]);
+  const [status, setstatus] = useState<boolean>();
 
+  useEffect(() => {
+    libraryServive.getAllAlbums().then(res => {
+      setsongsLibrary(Array.from(res.values()));
+    });
+  }, []);
 
+  const onSelectChange = (selectedRowKeys: any, selectedRows: any) => {
+    const data: any = [];
+    const Selected = Object.assign({}, selectedRows);
+    data.push(Selected);
+    console.log(data);
+  };
 
+  const getPlayList = (e: any) => {
+    setalbum(e.target.value);
+  };
 
-    useEffect(() => {
-        libraryServive.getAllAlbums().then(res =>{
-            setsongsLibrary(Array.from(res.values()));
-        })
-      }, []);
+  const savePlaylist = () => {
+    const saveData = {
+      name: album,
+      songs: selectedsongsLibrary
+    };
+    libraryServive
+      .savePlayLists(saveData)
+      .then(res => {
+        message.success(
+          <span>
+            {"PlayList Sucessfully Created, Please Visit Songs Library"}{" "}
+            <Button
+              type="link"
+              icon="close-circle"
+              onClick={() => message.destroy()}
+            />
+          </span>,
+          8
+        );
+        return false;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-      const onSelectChange =(selectedRowKeys: any, selectedRows: any)=> {
-        const data:any = [];
-       const Selected = Object.assign({},selectedRows);
-             data.push(Selected);
-             console.log(data)
-      }
+  const rowSelection = {
+    onChange: (selectedRowKeys: any, selectedRows: any) => {
+      const songId: any = [];
+      selectedRows.forEach((element: any) => {
+        songId.push(element["id"]);
+      });
+      setselectedsongsLibrary(songId);
+      console.log(
+        `selectedRowKeys: ${selectedRowKeys}`,
+        "selectedRows: ",
+        selectedRows
+      );
+    },
+    getCheckboxProps: (record: any) => ({
+      disabled: record.name === "Disabled User",
+      name: record.name
+    })
+  };
 
-      const getPlayList =(e:any)=> {
-        setalbum(e.target.value);
-      }
+  const handleChange = (pagination: any, filters: any, sorter: any) => {
+    setstatus(true);
+    console.log(filters);
+    setfiltered(filters);
+  };
 
-       const savePlaylist =()=> {
-           const saveData = {
-               name : album,
-               songs : selectedsongsLibrary
-              }
-              libraryServive.savePlayLists(saveData).then(res=>{
-                message.success(<span>{'PlayList Sucessfully Created, Please Visit Songs Library'}  <Button type="link" icon="close-circle" onClick={() => message.destroy()} /></span>, 8);
-                return false
-              }).catch((err)=>{
-                  console.log(err);
-              })
-                 
-       } 
+  let uniqueId = 0;
+  const getFilertitle = filterUtil.getFilters(songsLibrary, "title");
+  const getFileralbum = filterUtil.getFilters(songsLibrary, "album");
+  const getFilerartist = filterUtil.getFilters(songsLibrary, "artist");
 
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+      filters: getFilertitle,
+      filteredValue: status ? (filtered ? "" || null : null) : null,
+      onFilter: (value: any, record: any) => record.title.includes(value),
+      width: 250
+    },
+    {
+      title: "Album",
+      dataIndex: "album",
+      key: "album",
+      filters: getFileralbum,
+      filteredValue: status ? (filtered ? "" || null : null) : null,
+      onFilter: (value: any, record: any) => record.album.includes(value),
+      width: 250
+    },
+    {
+      title: "Artist",
+      dataIndex: "artist",
+      key: "artist",
+      filters: getFilerartist,
+      filteredValue: status ? (filtered ? "" || null : null) : null,
+      onFilter: (value: any, record: any) => record.artist.includes(value),
+      width: 250
+    },
+    {
+      title: "Duration",
+      dataIndex: "duration",
+      key: "duration"
+    }
+  ];
+  return (
+    <div>
+      <p>PlayList Name : {album}</p>
 
-      const rowSelection = {
-           onChange: (selectedRowKeys:any, selectedRows:any) => 
-           {  
-            const songId:any = [];
-            selectedRows.forEach((element:any) => {
-                songId.push(element['id'])
-            });
-            setselectedsongsLibrary(songId);
-            console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows); },
-            getCheckboxProps: (record:any) => ({ disabled: record.name === 'Disabled User',  name: record.name, }), 
-           
-        };
-    
-
-      const  handleChange = (pagination: any, filters: any, sorter: any) =>{
-           setstatus(true);
-           console.log(filters);
-           setfiltered(filters);
-
-      }
-
-
-      
-     let uniqueId = 0;
-     const getFilertitle = filterUtil.getFilters(songsLibrary, 'title');
-     const getFileralbum = filterUtil.getFilters(songsLibrary, 'album');
-     const getFilerartist = filterUtil.getFilters(songsLibrary, 'artist');
-
-      const columns = [
-        { 
-          title: 'Title',
-          dataIndex: 'title',
-          key: 'title',
-          filters: getFilertitle,
-          filteredValue : (status) ? (filtered) ? '' || null : null : null,
-          onFilter: (value: any, record: any) =>
-           record.title.includes(value),
-          width: 250
-        },
-        {
-            title: 'Album',
-            dataIndex: 'album',
-            key: 'album',
-            filters: getFileralbum,
-            filteredValue : (status) ? (filtered) ? '' || null : null : null,
-            onFilter: (value: any, record: any) =>
-             record.album.includes(value),
-            width: 250
-        },
-        {
-            title: 'Artist',
-            dataIndex: 'artist',
-            key: 'artist',
-            filters: getFilerartist,
-            filteredValue : (status) ? (filtered) ? '' || null : null : null,
-            onFilter: (value: any, record: any) =>
-             record.artist.includes(value),
-            width: 250
-        },
-        {
-            title: 'Duration',
-            dataIndex: 'duration',
-            key: 'duration'
-        }]
-      return (
-        <div>
-            <p>PlayList Name : {album}</p>
-
-         <Form>
-         <Form.Item
-        label="Play List Name"
-        required
-      >
-        <Input onChange = {getPlayList}/>
+      <Form>
+        <Form.Item label="Play List Name" required>
+          <Input onChange={getPlayList} />
         </Form.Item>
-         </Form>
-         <p> Library of Songs with Filters</p>
-         <Table
-    columns={columns} 
-    dataSource={songsLibrary}
-    rowSelection={rowSelection}
-    onChange = {handleChange}
-    bordered 
-    rowKey={(record:any) => {
-        if (!record.__uniqueId) record.__uniqueId = ++uniqueId;
-        return record.__uniqueId;
-      }} />
-      <Button className = "primary" onClick= {savePlaylist}>Save PlayList</Button>
-        </div>
-      )
+      </Form>
+      <p> Library of Songs with Filters</p>
+      <p>Please Check your favourite song's to Add to the new Playist </p>
 
-
+      <Table
+        columns={columns}
+        dataSource={songsLibrary}
+        rowSelection={rowSelection}
+        onChange={handleChange}
+        bordered
+        rowKey={(record: any) => {
+          if (!record.__uniqueId) record.__uniqueId = ++uniqueId;
+          return record.__uniqueId;
+        }}
+      />
+      <Button className="primary" onClick={savePlaylist}>
+        Save PlayList
+      </Button>
+    </div>
+  );
 };
 
 export default CreatePlaylist;
--- frontend/src/components/servicecomponents/SearchSong/Searchsong.tsx
+++ frontend/src/components/servicecomponents/SearchSong/Searchsong.tsx
@@ -1,82 +1,117 @@
-import React, { useState } from "react"
-import { Form, Input, Row, Col, Card, Avatar, Button, Tag, message, InputNumber } from "antd"
+import React, { useState } from "react";
+import {
+  Form,
+  Input,
+  Row,
+  Col,
+  Card,
+  Avatar,
+  Button,
+  Tag,
+  message,
+  InputNumber
+} from "antd";
 import Meta from "antd/lib/card/Meta";
 import { PlayListService } from "../../../services/PlayListService";
 
-
 export interface SearchsongProps {}
-const SearchSong = (props :SearchsongProps) => {
-    const [searchSong, setsearchSong] = useState({});
-    const [name, setname] = useState<string>('');
-    const [album, setalbum] = useState<string>('');
-    const [duration, setduration] = useState<string>('');
-    const [artist, setartist] = useState<string>('');
-    const [songsresAray, setsongsresAray] = useState([]);
-    const [id, setId] = useState<string>('');
-    const [Song, setSong] = useState<string>('');
+const SearchSong = (props: SearchsongProps) => {
+  const [searchSong, setsearchSong] = useState({});
+  const [name, setname] = useState<string>("");
+  const [album, setalbum] = useState<string>("");
+  const [duration, setduration] = useState<string>("");
+  const [artist, setartist] = useState<string>("");
+  const [songsresAray, setsongsresAray] = useState([]);
+  const [id, setId] = useState<string>("");
+  const [Song, setSong] = useState<string>("");
 
-    const libraryServive = new PlayListService();
+  const libraryServive = new PlayListService();
 
-  
-    const getSong =(e:any)=> {
-      console.log(e);
-      if(!e){
-        message.error(<span>{"Please Enter song ID"}  <Button type="link" icon="close-circle" onClick={() => message.destroy()} /></span>, 8);
-       }else{
-        libraryServive.getsongbyInput(e).then((res:any)=>{
-          if(res){
+  const getSong = (e: any) => {
+    console.log(e);
+    if (!e) {
+      message.error(
+        <span>
+          {"Please Enter song ID"}{" "}
+          <Button
+            type="link"
+            icon="close-circle"
+            onClick={() => message.destroy()}
+          />
+        </span>,
+        8
+      );
+    } else {
+      libraryServive
+        .getsongbyInput(e)
+        .then((res: any) => {
+          if (res) {
             setname(res.title);
             setalbum(res.album);
             setduration(res.duration);
-            setartist(res.artist); 
-          }else{
-            message.error(<span>{"No Songs Found"}  <Button type="link" icon="close-circle" onClick={() => message.destroy()} /></span>, 8);
-
+            setartist(res.artist);
+          } else {
+            message.error(
+              <span>
+                {"No Songs Found"}{" "}
+                <Button
+                  type="link"
+                  icon="close-circle"
+                  onClick={() => message.destroy()}
+                />
+              </span>,
+              8
+            );
           }
-
-        }).catch((err:any)=>{
-            message.error(<span>{err}  <Button type="link" icon="close-circle" onClick={() => message.destroy()} /></span>, 8);
-
         })
-      }
+        .catch((err: any) => {
+          message.error(
+            <span>
+              {err}{" "}
+              <Button
+                type="link"
+                icon="close-circle"
+                onClick={() => message.destroy()}
+              />
+            </span>,
+            8
+          );
+        });
     }
- return(
+  };
+  return (
     <div>
       <Form>
-         <Form.Item
-        label="Search for a Song By ID"
-        required
-      >
-        <InputNumber onChange = {getSong}/>
+        <Form.Item label="Search for a Song By ID" required>
+          <InputNumber onChange={getSong} />
         </Form.Item>
-         </Form> 
+      </Form>
 
-         <div  className= "playlist">
-          {
-           <div className="site-card-wrapper"><Row gutter={16}><Col span={8}><Card style={{ width: 300, marginTop: 16 }} loading = {false}>
-           <Meta
-             avatar={
-               <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-             }
-             title={name}
-             description= {album}
-           />
-           <br/>
-           <p>Duration : {duration}</p>
-           <p>Artist : {artist}</p>
-         </Card></Col>
-         </Row></div>
-             
-              
-               
-           }
-            <br/>
+      <div className="playlist">
+        {
+          <div className="site-card-wrapper">
+            <Row gutter={16}>
+              <Col span={8}>
+                <Card style={{ width: 300, marginTop: 16 }} loading={false}>
+                  <Meta
+                    avatar={
+                      <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                    }
+                    title={name}
+                    description={album}
+                  />
+                  <br />
+                  <p>Duration : {duration}</p>
+                  <p>Artist : {artist}</p>
+                </Card>
+              </Col>
+            </Row>
           </div>
-    </div>  
-
-     
- )
-
-}
+        }
+        <br />
+      </div>
+    </div>
+  );
+};
 
 export default SearchSong;
